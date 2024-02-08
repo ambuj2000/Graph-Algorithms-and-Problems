8e436a12eabd3f98f3562af4bfad0b5d920ccd49
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2024-02-08 10:34:58 +0530

    Update Dijkstra's algo using priority queue.cpp

--- Dijkstra's algo using priority queue.cpp
+++ Dijkstra's algo using priority queue.cpp
@@ -76,4 +76,30 @@ class Solution
     }
 };
 
+int main()
+{
+    // Driver code.
+    int V = 3, E = 3, S = 2;
+    vector<vector<int>> adj[V];
+    vector<vector<int>> edges;
+    vector<int> v1{1, 1}, v2{2, 6}, v3{2, 3}, v4{0, 1}, v5{1, 3}, v6{0, 6};
+    int i = 0;
+    adj[0].push_back(v1);
+    adj[0].push_back(v2);
+    adj[1].push_back(v3);
+    adj[1].push_back(v4);
+    adj[2].push_back(v5);
+    adj[2].push_back(v6);
+
+    Solution obj;
+    vector<int> res = obj.dijkstra(V, adj, S);
+
+    for (int i = 0; i < V; i++)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
 T.C-->E log V
